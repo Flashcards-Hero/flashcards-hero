150722c76bdeb3cb0efecce2b123888fcefd0f49
mereluo <meredithluo710@gmail.com>
2023-07-20 14:41:47 -0700

    Create onSubmit function

--- src/App.js
+++ src/App.js
@@ -1,29 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-import Upload from '../src/components/Upload'
+import React, { useState } from "react";
+import "./App.css";
+import Upload from "../src/components/Upload";
 
 function App() {
-  return (
-    <>
-      <Upload/>
-    </>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-  );
+    const [jsonOutput, setJsonOutput] = useState("");
+    const [result, setResult] = useState();
+
+    async function onSubmit(event) {
+        event.preventDefault();
+        console.log(jsonOutput);
+        try {
+            const response = await fetch("http://localhost:5000/generate", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: jsonOutput,
+            });
+            const data = await response.json();
+            if (response.status !== 200) {
+                throw data.error || new Error(`Request failed with status ${response.status}`);
+            }
+            setResult(data.result);
+            console.log(result);
+        } catch (error) {
+            // Consider implementing your own error handling logic here
+            console.error(error);
+            alert(error.message);
+        }
+    }
+    return (
+        <>
+            <Upload jsonOutput={jsonOutput} setJsonOutput={setJsonOutput} />
+            <button onClick={onSubmit}>Generate</button>
+        </>
+    );
 }
 
 export default App;
