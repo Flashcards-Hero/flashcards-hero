3d84b65ecf7d0c198cab9bff3cccddb87653e991
mereluo <meredithluo710@gmail.com>
2023-07-26 10:09:49 -0700

    Combined slider and vertical view

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -7,6 +7,7 @@ const Upload = () => {
     const [resultTxt, setResultTxt] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
     const [type, setType] = useState("");
+    const [flip, setFlip] = useState(false);
     const [currentCardIndex, setCurrentCardIndex] = useState(0);
 
     const handleConvert = async (file) => {
@@ -110,15 +111,18 @@ const Upload = () => {
 
     const handleNextCard = () => {
         if (currentCardIndex < resultTxt.length - 1) {
+            setFlip(false);
             setCurrentCardIndex((prevIndex) => prevIndex + 1);
         }
     };
 
     const handlePreviousCard = () => {
         if (currentCardIndex > 0) {
+            setFlip(false);
             setCurrentCardIndex((prevIndex) => prevIndex - 1);
         }
     };
+
     return (
         <>
             <div className="row p-5 bg-blue-50">
@@ -155,28 +159,31 @@ const Upload = () => {
             </div> */}
             <br />
             <div className="font-bold text-xl mb-2 pl-5">Result:</div>
-            {/* flash card ui */}
+            {/* flash card slider ui */}
             {!resultTxt ? (
-                <p>Generate your flashcards now!</p>
+                <div className="flex justify-center items-center">
+                    <p className="text-lg">Generate your flashcards now!</p>
+                </div>
             ) : (
                 <div className="flashcards">
                     <div className="offset-md-2 col-md-4">
-                        <FlashCard id={currentCardIndex} question={resultTxt[currentCardIndex].question} answer={resultTxt[currentCardIndex].answer} />
+                        <FlashCard id={currentCardIndex} question={resultTxt[currentCardIndex].question} answer={resultTxt[currentCardIndex].answer} setFlip={setFlip} status={flip} />
                     </div>
-                    <div className="flex justify-center">
-                        <button onClick={handlePreviousCard} disabled={currentCardIndex === 0} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4">
+                    <div className="row next-pre-button-group flex z-50 mb-5 offset-md-3">
+                        <button onClick={handlePreviousCard} disabled={currentCardIndex === 0} className="col-sm-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-10">
                             Previous
                         </button>
-                        <button onClick={handleNextCard} disabled={currentCardIndex === resultTxt.length - 1} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4">
+                        <button onClick={handleNextCard} disabled={currentCardIndex === resultTxt.length - 1} className="col-sm-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-20">
                             Next
                         </button>
                     </div>
                 </div>
             )}
+            {/* flash card vertical ui */}
             {resultTxt != null &&
                 resultTxt.map((item, index) => (
                     <>
-                        <div className="offset-md-2 col-md-4">
+                        <div className="offset-md-2">
                             <FlashCard id={index} question={item.question} answer={item.answer} />
                         </div>
                     </>
